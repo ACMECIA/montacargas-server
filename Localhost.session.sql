01e14086bc928fb046b892263375b0c4fd009f8d
acme <acme@acmecia.com>
2024-01-08 12:20:50 -0500

    u

--- Localhost.session.sql
+++
@@ -1,9 +0,0 @@
-
--- @block
-INSERT INTO `users` (`id`, `email`, `username`, `password`, `rol`)
-VALUES (NULL, 'cesar.yohan@kmmp.com.pe', 'cesary', '123456', 'client')
-
--- @block
--- Get email users by rols "client"
-
-SELECT `email` FROM `users` WHERE `rol` = 'client'
--- localhost.session.sql
+++ localhost.session.sql
@@ -1,5 +1,10 @@
 
 
 -- @block
+INSERT INTO `users` (`id`, `email`, `username`, `password`, `rol`)
+VALUES (NULL, 'cesar.yohan@kmmp.com.pe', 'cesary', '123456', 'client')
 
-SELECT * FROM local_data ORDER BY date ASC;
+-- @block
+-- Get email users by rols "client"
+
+SELECT `email` FROM `users` WHERE `rol` = 'client'
